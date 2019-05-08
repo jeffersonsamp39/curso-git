c75602a3b175c5739a48e16e37470211c1754ada
Jefferson Sousa Sampaio <jeffersonsamp@hotmail.com>
2019-05-08 11:38:00 -0300

    Projeto iniciado com a mensagem Ola mundo

---
+++ Projeto_teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+            Console.WriteLine();
+        }
+    }
+}
